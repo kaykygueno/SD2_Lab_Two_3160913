5ddb4c37da253b66e4c78285a77bdbdec042d430
Kayky <kaykyvitorsouza.gueno@student.griffith.ie>
2026-02-26 03:43:37 +0000

    Adding stubs methods

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,5 +1,26 @@
 package griffith;
 
 public class Conversions {
+	
+	//Adding stubs methods each specific to the test cases in ConversionsTest.java
+	
+    public double euroToDollar(double euro) {
+        return 0.0;
+    }
 
+    public double dollarToEuro(double dollar) {
+        return 0.0;
+    }
+
+    public int stringToInteger(String val) {
+        return 0;
+    }
+
+    public String integerToString(int val) {
+        return null; 
+    }
+
+    public String switchCase(String val) {
+        return null;
+    }
 }
