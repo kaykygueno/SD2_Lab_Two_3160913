33176482ea4b3ba95c1b866d4b1fe992f3184a0e
Kayky <kaykyvitorsouza.gueno@student.griffith.ie>
2026-03-02 21:09:17 +0000

    Second test Failed

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class ConversionsTest {
 	
-	//First test method that should fail
+	//First test Euro to dollar correctly
 	@Test
 	public void testEuroToDollar() {
 	    Conversions conv = new Conversions();
@@ -23,5 +23,22 @@ class ConversionsTest {
 	    // Test zero
 	    assertEquals(0.0, conv.euroToDollar(0.0), 0.01);
 	}
+	
+	//Second test to convert from dollar to euro method that should fail
+	@Test
+	public void testDollarToEuro() {
+	    Conversions conv = new Conversions();
 
+	    // Test Positive
+	    assertEquals(0.93, conv.dollarToEuro(1.0), 0.01);
+
+	    // Test larger positive
+	    assertEquals(92.59, conv.dollarToEuro(100.0), 0.01);
+
+	    // Test negative
+	    assertEquals(-0.93, conv.dollarToEuro(-1.0), 0.01);
+	    
+	    // Test zero
+	    assertEquals(0.0, conv.dollarToEuro(0.0), 0.01);
+	}
 }
